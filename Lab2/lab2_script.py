28b63acec608f92d9e7d46214ef28e7788a6079b
ruben <s236819@studenti.polito.it>
2020-11-27 16:13:24 +0100

    lab2 pt 1 implemented, must be verified

--- Lab2/lab2_script.py
+++ Lab2/lab2_script.py
@@ -1,1 +1,60 @@
 import numpy as np
+import random
+import math
+from scipy.stats import t
+
+
+# initial parameters
+runs = 100
+seed = 22
+m_people = 23
+n_elements = 365
+ci_level = 0.95
+
+class BirthdayParadoxSimulator:
+    def __init__(self, n_elements, runs, seed, ci_level):
+        #self.m_people = m_people
+        self.n_elements = n_elements
+        self.runs = runs
+        self.seed = seed
+        self.ci_level = ci_level
+
+    def retrieve_ci(self, mean, stddev):
+        t_treshold = t.ppf((self.ci_level + 1) / 2, df= self.runs-1)
+        ci = t_treshold * stddev /np.sqrt(self.runs)
+        rel_err = ci/ mean
+        return ci, rel_err
+
+    def run(self, m_people):
+        random.seed(a=self.seed)
+        # initialize the vector of n element with all 0
+        conflict_per_run = np.full(self.runs, 0)        
+        for run in range(self.runs):
+            chosen_element = np.full(self.n_elements, 0)
+            for _ in range(m_people):
+                rnd_element = random.randint(0, self.n_elements-1)
+                if chosen_element[rnd_element] != 0:
+                    conflict_per_run[run] = 1
+                    break
+                else:
+                    chosen_element[rnd_element] = 1
+        # return the probability of conflicts for the given setting
+        p_conflicts = float(np.sum(conflict_per_run) / self.runs) # it is also the mean
+        std_dev = math.sqrt(p_conflicts * (1 - p_conflicts)) # standar deviation
+        theoretical_probability = 1 - math.exp(-(m_people**2 / (2 * self.n_elements)))
+        ci, rel_err = self.retrieve_ci(p_conflicts, std_dev)
+        return m_people, theoretical_probability, p_conflicts,\
+                    p_conflicts - ci, p_conflicts + ci, rel_err
+    
+
+
+sim = BirthdayParadoxSimulator(n_elements, runs, seed, ci_level)
+#print("Probability of conflicts vs theoretical: ", sim.run(m_people))
+datafile = open("Lab2/birthdayparadox"+str(n_elements)+"elements.dat", "w")
+print("# peoples\tTheoretical\tSimulations\tciLow\tciHigh\tRelErr", file=datafile)
+for m in range(1, n_elements):
+    print("Running for m=", m)
+    out_run = sim.run(m)
+    print(*out_run, sep="\t", file=datafile)
+datafile.close()
+            
--- Lab3/lab3_spript.py
+++ Lab3/lab3_spript.py
@@ -15,19 +15,22 @@ with open("Lab3/words_alpha.txt", "r") as txt:
 start = time.time()
 with open("Lab3/words_alpha.txt", "r") as txt:
     words = csv.reader(txt)        
-    for b in range(1, w):
+    for b in range(50, w):
             collision = False
             words_set = set() # initialize the set
             for word in words:
-                word_hash = hashlib.md5(word[0].encode("utf-8")) # md5 hash
-                word_hash = int(word_hash.hexdigest(), 16) # cast integer format
-                h = word_hash % 2**b # map into a given range [0, 2^b - 1]
-                if h not in words_set:
-                    words_set.add(h) # add h in the set
-                else:
-                    collision = True
-                    print("A collision has been experienced with {} bits for the fingerprinting".format(b))
+                if collision == True:
                     break
+                else:
+                    word_hash = hashlib.md5(word[0].encode("utf-8")) # md5 hash
+                    word_hash = int(word_hash.hexdigest(), 16) # cast integer format
+                    h = word_hash % 2**b # map into a given range [0, 2^b - 1]
+                    if h not in words_set:
+                        words_set.add(h) # add h in the set
+                    else:
+                        collision = True
+                        print("A collision has been experienced with {} bits for the fingerprinting".format(b))
+                        break    
                 
 
 end = time.time()
