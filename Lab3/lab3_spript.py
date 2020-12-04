b91f215579f060f711628d190f422bd95c99b465
ruben <s236819@studenti.polito.it>
2020-12-04 15:17:06 +0100

    Fingerprinting exercise completed

--- Lab3/lab3_spript.py
+++ Lab3/lab3_spript.py
@@ -3,6 +3,7 @@ import hashlib
 import time
 import math
 from pympler import asizeof
+import pandas as pd
 
 # read the txt file
 w = 0 # initialize the counter
@@ -20,6 +21,7 @@ print("We have to deal with {} words".format(w))
 start = time.time()
 
 fingerprint_set = set() # initialize the set       
+size_empty_set = asizeof.asizeof(fingerprint_set)
 for b in range(1, w):
     for word in words_set:
         word_hash = hashlib.md5(word.encode("utf-8")) # md5 hash
@@ -44,17 +46,39 @@ for b in range(1, w):
 b_theo = math.log((w / epsilon), 2) # theoretical bits requirements
 print("\nThe number of bits needed for get a probability\n of fingerprint collision < 0.5 is {} bits".format(int(b_theo)))
 
+
+# THEORETICAL STORAGE
+# for the fingerprint set we have to store b_exp bits for each word
+# a good avg size for a string could be 50 bytes
+fing_set_theory_storage = w * b_exp # this is in bits
+words_set_theory_storage = w * 50 # this is in bytes
+print("\nTheoretical storage for words set = {:.2f} KB".format(words_set_theory_storage/1024))
+print("Theoretical storage for {}-fingerprint set = {:.2f} KB".format(b_exp, fing_set_theory_storage/(8*1024)))
+
 # ACTUAL STORAGE
 # first put the word in a words set
 string_set_storage = asizeof.asizeof(words_set)
 hash_set_storage = asizeof.asizeof(fingerprint_set)
-print("\nStoring the strings in a set requires: {:.2f} KB".format(string_set_storage/1024))
-print("Storing the fingerprints in a set requires: {:.2f} KB".format(hash_set_storage/1024))
+print("\nSize of an empty set in python: {:.2f} KB".format(size_empty_set/1024))
+print("Storing the strings in a set takes: {:.2f} KB".format(string_set_storage/1024))
+print("Storing the fingerprints in a set takes: {:.2f} KB".format(hash_set_storage/1024))
 print("With a space saving of {:.2f}x".format(string_set_storage/hash_set_storage))
 
-
-
+# P(FP) = 1-(1-(1/n))^m
+n = 2**b_exp
+p_false_positive = 1-(1-(1/n))**w
+print("\nWith the Fingerprint whose range is [0, 2^{}-1] P(FP) = {:.6f}%".format(b_exp, p_false_positive*100))
 
 print("\nThe set contains: ",len(fingerprint_set))
 end = time.time()
 print("Execution time: {:.2f} s".format(end-start))
+
+print("\nTo resume:\n")
+df = pd.DataFrame(data={'Storage':['Word set', 'Fingerprint set'],
+                        'Bits per fingerprint':['N.A.', b_exp],
+                        'Prob. false positive [%]':['N.A.', p_false_positive*100],
+                        'Min theoretical memory [KB]':[words_set_theory_storage/1024, fing_set_theory_storage/(1024*8)],
+                        'Actual memory [KB]':[string_set_storage/1024, hash_set_storage/1024]}, 
+                )
+
+print(df.set_index('Storage'))
